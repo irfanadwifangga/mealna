846467099f267bcf366e98e9b1235d40f98be631
NagatsuAkiza <nagatsuakiza@gmail.com>
2025-11-03 12:55:06 +0700

    Add loading screen on startup

---
+++ app/src/main/java/com/example/mealna/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mealna;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // hide action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // loading screen
+        handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+                intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+        }, 3000);
+    }
+}
